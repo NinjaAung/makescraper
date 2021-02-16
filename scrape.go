9d7a9ec14ca57c2cc0e78aa48105f57046c0d3c6
NinjaAung <ninja.aung02@gmail.com>
2021-02-15 23:37:54 -0800

    OnHtml is a simmilar function of ForEach
    No need for list since Onhtml will only return a single container

--- scrape.go
+++ scrape.go
@@ -23,9 +23,9 @@ func main() {
 	// a.SQnoC3ObvgnGjWt90zD9Z
 	// div._1oQyIsiPHYt6nx7VOmd1sz.bE7JgM2ex7W3aF3zci5bm.D3IyhBGwXo9jPwz-Ka0Ve
 	c.OnHTML("div._1oQyIsiPHYt6nx7VOmd1sz.bE7JgM2ex7W3aF3zci5bm.D3IyhBGwXo9jPwz-Ka0Ve", func(e *colly.HTMLElement) {
-		findTitleAndLink(e)
-		//findVotes(e)
-		findFlair(e)
+		print(findTitleAndLink(e))
+		print(findVotes(e))
+		print(findFlair(e))
 	})
 	// Before making a request print "Visiting ..."
 	c.OnRequest(func(r *colly.Request) {
@@ -36,21 +36,18 @@ func main() {
 	c.Visit("https://new.reddit.com/r/wallstreetbets/search/?q=-flair%3AMeme%20-flair%3ASatire%20-flair%3AShitpost&restrict_sr=1&t=day&sort=hot")
 }
 
-func findTitleAndLink(e *colly.HTMLElement) (titles, links []string) {
+func findTitleAndLink(e *colly.HTMLElement) (title, link string) {
 	container := e
 	container.ForEach("a.SQnoC3ObvgnGjWt90zD9Z", func(_ int, elem *colly.HTMLElement) {
-		titles = append(titles, string(elem.Text))
-		links = append(links, string(elem.Attr("href")))
+		title = elem.Text
+		link = elem.Attr("href")
 
 	})
-	return titles, links
+	return title, link
 }
 
-func findVotes(e *colly.HTMLElement) []int64 {
-	var (
-		vote  int64
-		votes []int64
-	)
+func findVotes(e *colly.HTMLElement) (vote int64) {
+
 	e.ForEach("div._1rZYMD_4xY3gRcSS3p8ODO._3a2ZHWaih05DgAOtvu6cIo", func(_ int, elem *colly.HTMLElement) {
 		voteString := elem.Text
 
@@ -68,16 +65,15 @@ func findVotes(e *colly.HTMLElement) []int64 {
 
 			}
 		}
-		votes = append(votes, vote)
 	})
-	return votes
+	return vote
 }
 
-func findFlair(e *colly.HTMLElement) (flairs []string) {
+func findFlair(e *colly.HTMLElement) (flair string) {
 	e.ForEach("div._2X6EB3ZhEeXCh1eIVA64XM span", func(_ int, elem *colly.HTMLElement) {
-		flairs = append(flairs, string(elem.Text))
+		flair = elem.Text
 	})
-	return flairs
+	return flair
 }
 
 func check(err error) {
