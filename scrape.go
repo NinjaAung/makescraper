cd077cc3ced9d69397480bf8ba05f70ed3cd2407
NinjaAung <ninja.aung02@gmail.com>
2021-02-15 23:16:15 -0800

    Created Function For Struct Values

--- scrape.go
+++ scrape.go
@@ -2,29 +2,82 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/gocolly/colly"
 )
 
-// main() contains code adapted from example found in Colly's docs:
-// http://go-colly.org/docs/examples/basic/
+// Post is a representation of each reddit block
+type Post struct {
+	Title   string
+	Link    string
+	Upvotes int
+	Flair   string
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
 	// Instantiate default collector
 	c := colly.NewCollector()
-
-	// On every a element which has href attribute call callback
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-                link := e.Attr("href")
-
-		// Print link
-                fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+	// a.SQnoC3ObvgnGjWt90zD9Z
+	// div._1oQyIsiPHYt6nx7VOmd1sz.bE7JgM2ex7W3aF3zci5bm.D3IyhBGwXo9jPwz-Ka0Ve
+	c.OnHTML("div._1oQyIsiPHYt6nx7VOmd1sz.bE7JgM2ex7W3aF3zci5bm.D3IyhBGwXo9jPwz-Ka0Ve", func(e *colly.HTMLElement) {
+		findTitle(e)
+		findVotes(e)
+		findFlair(e)
 	})
-
 	// Before making a request print "Visiting ..."
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL.String())
 	})
 
-	// Start scraping on https://hackerspaces.org
-	c.Visit("https://hackerspaces.org/")
+	// Start scraping on
+	// https://new.reddit.com/r/wallstreetbets/search?sort=hot&restrict_sr=on&q=flair%3ADD&t=day
+	c.Visit("https://new.reddit.com/r/wallstreetbets/search/?q=-flair%3AMeme%20-flair%3ASatire%20-flair%3AShitpost&restrict_sr=1&t=day&sort=hot")
+}
+
+func findTitle(e *colly.HTMLElement) {
+	container := e
+	container.ForEach("a.SQnoC3ObvgnGjWt90zD9Z", func(_ int, elem *colly.HTMLElement) {
+		fmt.Print(elem.Text)
+		fmt.Println(elem.Attr("href"))
+
+	})
+
+}
+
+func findVotes(e *colly.HTMLElement) {
+	//var votes []int
+	e.ForEach("div._1rZYMD_4xY3gRcSS3p8ODO._3a2ZHWaih05DgAOtvu6cIo", func(_ int, elem *colly.HTMLElement) {
+		var err error
+		var vote int64
+		voteString := elem.Text
+
+		if voteString == "Vote" {
+			vote = 0
+		} else {
+
+			if string(voteString[len(elem.Text)-1]) == "k" {
+				votenum, err := strconv.ParseFloat(string(voteString[:len(elem.Text)-1]), 64)
+				check(err)
+				vote = int64(votenum * float64(1000))
+			} else {
+				vote, err = strconv.ParseInt(voteString, 10, 0)
+				check(err)
+
+			}
+		}
+		fmt.Println(vote)
+	})
+}
+
+func findFlair(e *colly.HTMLElement) {
+	e.ForEach("div._2X6EB3ZhEeXCh1eIVA64XM span", func(_ int, elem *colly.HTMLElement) {
+		fmt.Print(elem.Text)
+	})
 }
